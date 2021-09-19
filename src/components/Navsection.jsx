2cd136d22547fb72591cf2ebcd390643680c7311
Sandeep <sandeepnandanwar92@gmail.com>
2021-09-19 22:40:01 +0530

    first commit

--- src/components/Navsection.jsx
+++ src/components/Navsection.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export default function Navsection(props) {
     const { cartItems, onRemove, cartItemlength } = props
+    const {
+        buttonLabel,
+        className
+    } = props;
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    const externalCloseBtn = <button className="close" style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={toggle}>&times;</button>;
     return (
         <>
             <div className="navbars">
@@ -10,29 +21,35 @@ export default function Navsection(props) {
                         <p className="font-bold">Start Bootstrap</p>
                     </div>
                     <div className="btn-group dropstart">
-                        <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span className="cart-area"><i className="fas fa-shopping-cart"></i><span className="cart mx-2 text-bold">Cart</span><span className="cart-no mx-2 text-white">{cartItemlength}</span></span>
-                        </button>
-                        <ul className="dropdown-menu">
-                            {cartItems.map((item, key) => {
-                                const { image, type, price } = item;
-                                return (
-                                    <div className=" container item" key={key}>
-                                        <div>
-                                            <img src={image} className="card-img-top card-img" alt="..." style={{ "width": "50px" }} />
-                                        </div>
-                                        <div className="container">
-                                            <h6 className="card-type">{type}</h6>
-                                        </div>
-                                        <h6 className=" card-price">{price}</h6>
-                                        <div>
-                                            <button className="btn btn-danger card-btn ml-3" onClick={() => onRemove(item)}><i className="fas fa-trash"></i></button>
-                                        </div>
-                                        <hr />
-                                    </div>
-                                )
-                            })}
-                        </ul>
+                        <Button color="white" className="ml-auto" onClick={toggle}>{buttonLabel}<span className="cart-area"><i className="fas fa-shopping-cart"></i><span className="cart mx-2 text-bold">Cart</span><span className="cart-no mx-2 text-white">{cartItemlength}</span></span></Button>
+                        <Modal isOpen={modal} toggle={toggle} className={className} external={externalCloseBtn}>
+                            <ModalHeader>My Cart</ModalHeader>
+                            <ModalBody>
+                                <div className="container ">
+                                    {cartItems.map((item, key) => {
+                                        const { image, type, price } = item;
+                                        return (
+                                            <div className=" container item" key={key}>
+                                                <div>
+                                                    <img src={image} className="card-img-top card-img" alt="..." style={{ "width": "50px" }} />
+                                                </div>
+                                                <div className="container">
+                                                    <h6 className="card-type">{type}</h6>
+                                                </div>
+                                                <h6 className=" card-price">{price}</h6>
+                                                <div>
+                                                    <button className="btn btn-danger card-btn mb-2 ml-3" onClick={() => onRemove(item)}><i className="fas fa-trash"></i></button>
+                                                </div>
+                                                <hr />
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="secondary" onClick={toggle}>Cancel</Button>
+                            </ModalFooter>
+                        </Modal>
                     </div>
                 </nav>
             </div>
